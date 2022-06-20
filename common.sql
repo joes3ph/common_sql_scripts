a241e23cdc4f52924abbd3459db6456d08ab3477
YONG FANG <89886972+leonfard@users.noreply.github.com>
2022-06-20 17:09:05 +0800

    Create common.sql

---
+++ common.sql
@@ -0,0 +1,5 @@
+# 批量kill带锁的会话
+SELECT 'alter system kill session '''|| SID || ',' || SERIAL# ||''';' FROM V$SESSION WHERE SID in (SELECT SESSION_ID FROM V$LOCKED_OBJECT, USER_OBJECTS WHERE V$LOCKED_OBJECT.OBJECT_ID = USER_OBJECTS.OBJECT_ID);
+
+# 批量kill所有的会话
+SELECT 'alter system kill session '''|| SID || ',' || SERIAL# ||''';' FROM V$SESSION;
